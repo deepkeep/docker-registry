43849b5343b56ed9ecd981b459a64dec1744011d
Fredrik Noren <noren@spotify.com>
2015-07-26 16:09:41 -0400

    Remove custom docker file

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM registry:2
-
-EXPOSE 5000
-
-COPY config.yaml cmd/registry/config.yml
--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 read -p "Enter AWS access key: " AWS_ACCESS_KEY
 read -p "Enter AWS secret key: " AWS_SECRET_KEY
-docker build -t deepkeep-docker-registry .
 docker run -d -p 443:443 \
     -v `pwd`/certs:/certs \
+    -e REGISTRY_NOTIFICATIONS_ENDPOINTS=[{ name: 'deepkeep-www', url: 'http://www.deepkeep.co/private/api/v1/dockerevents' }] \
+    -e REGISTRY_NOTIFICATIONS_ENDPOINTS_0_URL http://www.deepkeep.co/private/api/v1/dockerevents
     -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/docker.deepkeep.co.crt \
     -e REGISTRY_HTTP_TLS_KEY=/certs/docker.deepkeep.co.key \
     -e REGISTRY_STORAGE_S3_ACCESSKEY=$AWS_ACCESS_KEY \
     -e REGISTRY_STORAGE_S3_SECRETKEY=$AWS_SECRET_KEY \
+    -e REGISTRY_STORAGE_S3_BUCKET=deepkeep-docker-registry \
+    -e REGISTRY_STORAGE_S3_REGION=us-west-2 \
     --restart=always --name deepkeep-docker-registry \
-    deepkeep-docker-registry
+    registry:2
